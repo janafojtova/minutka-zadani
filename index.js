f62e42826c6e5be37b0ef32f9a1a233bf1750639
janafojtova <124291649+janafojtova@users.noreply.github.com>
2023-03-16 09:04:48 +0100

    vypracování úkolu minutka

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 console.log('funguju!');
+/*
+Minutka
+to dáš
+Naklonujte si repozitář pro stránku s kuchyňskou minutkou. Využijte funkci setTimeout ke splnění následujících
+ úkolů.
+
+Zařiďte, aby minutka začala zvonit za pět vteřin po otevření stránky. Zvonění zařídíte tak, že k elementu budíku 
+přídáte CSS třídu alarm--ring.
+Nechte uživatele skrze prompt zadat kolik vteřin má uplynout než minutka začne zvonit.
+Můžete zařídit i spuštění zvuku. Stačí ze stránky vybrat audio element a zavolat na něm metodu play. Abyste zvuk 
+slyšeli, musíte po zadání času na stránku kliknout. Prohlížeč Chrome totiž brání stránkam přehrávat audio či video
+ dokud uživatel se stránkou nějak neinteragoval.
+Přidejte na stránku tlačítko, které umožní odpočet minutky zrušit dřív, než začne zvonit.
+*/
+const pocetVterinOdUzivatele = prompt("Zadejte počet vteřin");
+
+const zacniZvonit = ()=>{
+    const budik = document.querySelector(".alarm");
+    budik.classList.add("alarm--ring")  
+}
+
+const audio =document.querySelector(".audio")
+const spustAudio =()=>{
+    audio.play()
+}
+const zastavAudio =()=>{
+    audio.pause()
+}
+const resetujAudio =()=>{
+    audio.currentTime=0
+}
+setTimeout(spustAudio,Number(pocetVterinOdUzivatele)*1000);
+   
